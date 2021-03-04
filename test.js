0e4308e8c45bd8abb931a94a4e718f8a5eb2ff60
LinDhan <1354273961@qq.com>
2021-03-04 15:45:04 +0800

    修改测试2.0

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 var fn1=function(){
-    console.log(123456)
+    console.log("fn1")/**修改内容 */
 }
 var fn2=function(){
-    console.log('fn2')
+    console.log('fn2') /**不动 */
+}
+var fn3=function(){
+    console.log('fn3')  /**新增 */
 }
